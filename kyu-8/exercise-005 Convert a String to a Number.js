00f61d6529078b5eb0734460dd6679bb7d8a8103
kkaluska <kkaluska>
2022-05-10 18:14:30 +0200

    Dodanie: zadanie 5

---
+++ kyu-8/exercise-005 Convert a String to a Number.js
@@ -0,0 +1,25 @@
+/*
+8 kyu: Convert a String to a Number!
+
+Link: https://www.codewars.com/kata/544675c6f971f7399a000e79
+Link solution: https://www.codewars.com/kata/544675c6f971f7399a000e79/solutions/javascript
+
+Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+"1234" --> 1234
+"605" --> 605
+"1405" --> 1405
+"-7" --> -7
+
+*/
+
+let stringToNumber = function (str) {
+  // put your code here
+  return Number(str);
+};
