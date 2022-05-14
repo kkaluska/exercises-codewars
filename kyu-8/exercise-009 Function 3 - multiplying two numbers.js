0e91fca5c7a235b836ed207d4ebddf51443ee2f6
kkaluska <kaskakaluska@gmail.com>
2022-05-14 12:34:20 +0200

    Dodanie zadania 7-9

---
+++ kyu-8/exercise-009 Function 3 - multiplying two numbers.js
@@ -0,0 +1,11 @@
+/*8 kyu
+
+https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+Function 3 - multiplying two numbers
+Implement a function which multiplies two numbers.
+*/
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
